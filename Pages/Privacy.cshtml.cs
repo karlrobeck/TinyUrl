de38a9a23dcdfe4cd4de56271278528d809eb00d
karl robeck alferez <karlalferezfx@gmail.com>
2026-03-04 15:01:11 +0800

    initial commit

---
+++ Pages/Privacy.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TinyUrl.Pages;
+
+public class PrivacyModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
+
